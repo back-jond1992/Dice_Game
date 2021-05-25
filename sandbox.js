5bc90b7a16ffae2fe36cf915d27907bc8fc15840
Jack Bond <b.jond7@outlook.com>
2021-05-25 22:22:52 +0100

    finisged roll dice functions

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,22 @@
-let randomNumber = Math.floor(Math.random() * 5
+let diceImages = new Array (
+    "images/dice1.png",
+    "images/dice2.png",
+    "images/dice3.png",
+    "images/dice4.png",
+    "images/dice5.png",
+    "images/dice6.png",
+)
 
-function rollDice() {
-    let diceImages = new Array (
-        "images/dice1.png",
-        "images/dice2.png",
-        "images/dice3.png",
-        "images/dice4.png",
-        "images/dice5.png",
-        "images/dice6.png",
-    )
-    document.getElementById('img1').setAttribute('src',diceImages[randomNumber])
+function rollDice1() {
+    let randomNumber = Math.floor(Math.random() * diceImages.length)
+    document.getElementById('img1').src = diceImages[randomNumber];
 }
+
+rollDice1()
+
+function rollDice2() {
+    let randomNumber = Math.floor(Math.random() * diceImages.length)
+    document.getElementById('img2').src = diceImages[randomNumber];
+}
+
+rollDice2()
